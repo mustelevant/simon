3ca3670761310f8471deca1ff81f00ed72468aa1
mustelevant <47430365+mustelevant@users.noreply.github.com>
2019-02-20 18:53:04 +0200

    Game complete
    All working!

--- game.js
+++ game.js
@@ -2,12 +2,14 @@ const buttonColors = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
 let userClickedPattern = [];
 let level = 0;
+let started = 0;
 
 // Generate a random number from 0-3, pick the color from buttonColors array and push the color to gamePattern array
 // Flash the chosen color and playSound
 // Change h1 text to current Level
 // Increase level by 1
 function nextSequence() {
+  userClickedPattern = [];
   const randomNumber = Math.floor(Math.random() * 4);
   const randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
@@ -18,8 +20,13 @@ function nextSequence() {
 }
 
 // Event listener for first keypress - Start game
-$("html").one("keypress", function() {
-  nextSequence();
+$("html").on("keypress", function() {
+  if (started === 0) {
+    nextSequence();
+    started = 1;
+  } else {
+    console.log("Game already in progress!");
+  }
 });
 
 // Detect which button is clicked and add the ID to userClickedPattern array + playSound() + animatePress()
@@ -28,7 +35,8 @@ $(".btn").on("click", function(event) {
   userClickedPattern.push(event.target.id);
   playSound(event.target.id);
   animatePress(event.target.id);
-  console.log(event.target.id);
+  // console.log(event.target.id);
+  checkAnwser(userClickedPattern.length - 1);
 });
 
 // Play sound and flash clicked box
@@ -43,4 +51,37 @@ function animatePress(currentColor) {
   setTimeout(function () {
     $("#" + currentColor).removeClass("pressed");
   }, 100);
+}
+
+// Check users anwser, game over if answer is wrong + startOver()
+function checkAnwser (currentLevel) {
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    console.log("Success");
+  } else {
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout (function () {
+      $("body").removeClass("game-over");
+    }, 400);
+    $("h1").text("Game Over! Press Any Key to Restart!");
+    startOver();
+    console.log("Game over");
+  }
+// Do nothing if user has not finished pattern
+// Wait 1 s and nextSequence() if pattern is finished correctly
+  if (userClickedPattern.length < gamePattern.length || userClickedPattern.length === 0) {
+    return null;
+  } else {
+    setTimeout (function () {
+      nextSequence();
+    }, 1000);
+  }
+}
+
+// Function to restart the game and reset progress
+function startOver () {
+  gamePattern = [];
+  userClickedPattern = [];
+  level = 0;
+  started = 0;
 }
